a3e05a205717226cd753d9fca4eed2008f185f84
omer <oemer.peynirci@ontavio.com>
2022-11-08 15:29:19 +0100

    cart login in

---
+++ cart/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Header from 'home/Header';
+import Footer from 'home/Footer';
+import CartContent from './CartContent';
+import "./index.scss";
+
+const App = () => (
+  <div className="mx-auto max-w-6xl">
+   <Header/>
+      <div className="my-10">
+      <CartContent/>
+
+      </div>
+      <Footer/>
+  </div>
+);
+ReactDOM.render(<App />, document.getElementById("app"));
